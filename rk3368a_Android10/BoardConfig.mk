b3bcbdf533164a55e8ce18de7f23701fe90b2870
TBM <mateotrueba@gmail.com>
2024-03-18 01:58:54 +0000

    Disable gyroscope and compass sensors support
    Change-Id: Ia926327f497baaf89a0a4689a130af3df44fd98a

--- rk3368a_Android10/BoardConfig.mk
+++ rk3368a_Android10/BoardConfig.mk
@@ -2,9 +2,7 @@ include device/rockchip/rk3368a/BoardConfig.mk
 
 BOARD_USES_AB_IMAGE := false
 BOARD_RECORD_COMMIT_ID := false
-BOARD_GYROSCOPE_SENSOR_SUPPORT := true
-BOARD_COMPASS_SENSOR_SUPPORT := true
-BOARD_SENSOR_COMPASS_AK8963-64 := true
+BOARD_SENSOR_COMPASS_AK8963-64 := false
 
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
 TARGET_RECOVERY_FSTAB := device/rockchip/rk3368a/rk3368_Android10/fstab.rk30board_AB
